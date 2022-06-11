88d70a4cc95d6326dc47ebb52880add254940d8f
Victor Henrique <victorhenrique455@gmail.com>
2022-06-10 23:38:23 -0300

    pagina inicial completa

---
+++ src/paginas/App.tsx
@@ -0,0 +1,15 @@
+import Formulario from '../componentes/formulario';
+import Lista from '../componentes/lista';
+
+import style from "./App.module.scss"
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Formulario/>
+      <Lista/>
+    </div>
+  );
+}
+
+export default App;
